90abc50d812a289768740613d01ff7ece0072001
Sayli Akarshe <sayli.akarshe@wineconx.com>
2025-04-24 14:47:06 +0200

    update task

--- agentToVote.py
+++ agentToVote.py
@@ -6,7 +6,7 @@ load_dotenv()
 
 async def main():
     agent = Agent(
-        task= "Vote for La Huella | Maldonado listed in restaurants, bars, & beach clubs in vote with execellent points, recently visited the place, https://www.cntraveler.com/story/vote-readers-choice-awards",
+        task= "Vote for La Susana Vik listed in restaurants, bars, & beach clubs in vote with execellent points, vote as recently visited this place,https://condenast-interactive.typeform.com/to/yJBJ9mfX?typeform-source=www.cntraveler.com",
         llm=ChatOpenAI(model="gpt-4o"),
     )
     await agent.run()
