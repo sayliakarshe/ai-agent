c0cd62b665be935c5c733948707885de68f6067a
Sayli Akarshe <sayli.akarshe@wineconx.com>
2025-05-01 12:05:11 +0200

    update task

--- agentToVote.py
+++ agentToVote.py
@@ -5,8 +5,10 @@ from dotenv import load_dotenv
 load_dotenv()
 
 async def main():
+    propertyName = "10 Barrel"
+    linkToOpen = "https://condenast-interactive.typeform.com/to/yJBJ9mfX?typeform-source=www.cntraveler.com"
     agent = Agent(
-        task= "Vote for La Susana Vik listed in restaurants, bars, & beach clubs in vote with execellent points, vote as recently visited this place,https://condenast-interactive.typeform.com/to/yJBJ9mfX?typeform-source=www.cntraveler.com",
+        task= "Open the given link"+ linkToOpen +"In the given link, step 1 please search for the property listed in restaurants, bars, & beach clubs. The name of property name is " + propertyName + "In step 2 please rate 'Excellent' in all the given categories. In step 3 please select anyone option and click 'OK' button. In step 4 please select yes. In step 5 do not leave any note for next travellers, keep it empty and click on 'OK' button. In step 6 click on 'No,submit my votes!' and then click on 'SUBMIT' button.",
         llm=ChatOpenAI(model="gpt-4o"),
     )
     await agent.run()
